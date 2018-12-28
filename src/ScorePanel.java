5fe53178122fbb94cb9de1e12b7b986c04615feb
Hannah <hannahmbryson@gmail.com>
2018-12-28 22:04:44 +0000

    Changing size of Scorepanel

--- src/ScorePanel.java
+++ src/ScorePanel.java
@@ -1,7 +1,5 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
 import java.io.IOException;
 import java.awt.image.BufferedImage;
 import java.awt.*;
@@ -9,7 +7,7 @@ import java.awt.*;
 class ScorePanel extends JPanel {
 
     private static final long serialVersionUID = 1L;
-    private static final int FRAME_WIDTH = 750, FRAME_HEIGHT = 750;  // must be even
+    private static final int FRAME_WIDTH = 250, FRAME_HEIGHT = 500;  // must be even
     private static final float COL_OFFSET = 52f, ROW_OFFSET = 29f;
     private static final float COL_SCALE = 26.9f, ROW_SCALE = 27.1f;
 
@@ -17,7 +15,7 @@ class ScorePanel extends JPanel {
 
     ScorePanel() {
         setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
-        setBackground(Color.WHITE);
+        setBackground(Color.blue);
         try {
             scoreImage = ImageIO.read(this.getClass().getResource("scoreBackground.jpg"));
         } catch (IOException ex) {
