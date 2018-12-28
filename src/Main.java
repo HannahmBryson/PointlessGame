cb90605e89418a3c7e5af80373d204319114d475
Hannah <hannahmbryson@gmail.com>
2018-12-28 20:11:10 +0000

    Creating stucture

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+        private final UI ui = new UI();
+
+        private void testUI() {
+            String command;
+
+            do {
+                command = ui.getCommand();
+                ui.displayString(command);
+                ui.display();
+            } while (!command.equals("quit"));
+        }
+
+        public static void main(String[] args) {
+            Main game = new Main();
+            game.testUI();
+            System.exit(0);
+        }
+}
