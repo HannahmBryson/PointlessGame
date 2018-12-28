aca02517d6a85651356a2f8c678cb4e871312a8e
Hannah <hannahmbryson@gmail.com>
2018-12-28 22:06:44 +0000

    Changing size of window

--- src/UI.java
+++ src/UI.java
@@ -3,24 +3,22 @@ import javax.swing.*;
 
 public class UI {
 
-    private static final int FRAME_WIDTH = 1200;
-    private static final int FRAME_HEIGHT = 800;
-
-    private final GamePanel gamePanel;
+    private static final int FRAME_WIDTH = 1150, FRAME_HEIGHT = 700;
+    
+    private final GamePanel gamePanel = new GamePanel();
     private final InfoPanel infoPanel = new InfoPanel();
     private final CommandPanel commandPanel = new CommandPanel();
     private final ScorePanel scorePanel = new ScorePanel();
 
     UI() {
         JFrame frame = new JFrame();
-        gamePanel = new GamePanel();
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setTitle("Pointless");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.add(gamePanel, BorderLayout.LINE_START);
-        frame.add(infoPanel, BorderLayout.LINE_END);
-        frame.add(commandPanel,BorderLayout.PAGE_END);
-        frame.add (scorePanel, BorderLayout.EAST)
+        frame.add(gamePanel, BorderLayout.WEST);
+        frame.add(scorePanel, BorderLayout.EAST);
+        frame.add(infoPanel, BorderLayout.SOUTH);
+        frame.add(commandPanel,BorderLayout.SOUTH);
         frame.setResizable(false);
         frame.setVisible(true);
     }
@@ -31,8 +29,10 @@ public class UI {
 
     public void display() {
         gamePanel.refresh();
+        scorePanel.refresh();
     }
 
     public void displayString(String string) {
         infoPanel.addText(string);
+    }
 }
