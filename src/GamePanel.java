9ac8e3cad00b904081d81aaa2cc5e9ff977df2ee
Hannah <hannahmbryson@gmail.com>
2018-12-28 22:03:40 +0000

    Set up Game panel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,2 +1,31 @@
-public class GamePanel {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import java.awt.*;
+
+class GamePanel extends JPanel {
+
+    private static final long serialVersionUID = 1L;
+    private static final int FRAME_WIDTH = 900, FRAME_HEIGHT = 500;  // must be even
+    private static final float COL_OFFSET = 52f, ROW_OFFSET = 29f;
+    private static final float COL_SCALE = 26.9f, ROW_SCALE = 27.1f;
+
+    private BufferedImage gameImage;
+
+    GamePanel() {
+        setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
+        setBackground(Color.BLACK);
+        try {
+            gameImage = ImageIO.read(this.getClass().getResource("gameBackground.jpg"));
+        } catch (IOException ex) {
+            System.out.println("Could not find the image file " + ex.toString());
+        }
+    }
+
+    public void refresh() {
+        revalidate();
+        repaint();
+    }
+
 }
